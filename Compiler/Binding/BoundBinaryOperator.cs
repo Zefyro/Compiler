436a1d9ca7947941dca75aad9b4a88a930f48ecb
Zefyro <71092443+Zefyro@users.noreply.github.com>
2025-08-12 02:23:23 +0300

    BinaryNotEqualsExpression for System.Doubles

--- Compiler/Binding/BoundBinaryOperator.cs
+++ Compiler/Binding/BoundBinaryOperator.cs
@@ -18,6 +18,7 @@ public sealed class BoundBinaryOperator(SyntaxKind syntaxKind, BoundKind kind, T
         new(SyntaxKind.StarStarToken, BoundKind.BinaryPowerExpression, typeof(double), typeof(double), typeof(double)),
 
         new(SyntaxKind.EqualsEqualsToken, BoundKind.BinaryEqualsExpression, typeof(double), typeof(double), typeof(bool)),
+        new(SyntaxKind.BangEqualsToken, BoundKind.BinaryNotEqualsExpression, typeof(double), typeof(double), typeof(bool)),
         new(SyntaxKind.LessthanToken, BoundKind.BinaryLessThanExpression, typeof(double), typeof(double), typeof(bool)),
         new(SyntaxKind.MorethanToken, BoundKind.BinaryGreaterThanExpression, typeof(double), typeof(double), typeof(bool)),
         new(SyntaxKind.LessthanEqualsToken, BoundKind.BinaryLessThanEqualsExpression, typeof(double), typeof(double), typeof(bool)),
